2b8521a92b53af56f4b481e471956b114697ac29
Amos Kenner <akenner92@gmail.com>
2022-08-19 14:37:54 -0500

    Allows player to add mark

--- script.js
+++ script.js
@@ -13,11 +13,20 @@ const gameBoard = (() => {
   board.forEach((item, index) => {
     const square = document.createElement("div");
     square.className = "square";
+    square.textContent = "";
     square.addEventListener("click", () => {
-      square.classList.add(game.activePlayer.marker);
-      square.textContent = game.activePlayer.marker;
-      board[index] = game.activePlayer.marker;
-      game.openSquares -= 1;
+      if (square.textContent === "") {
+        square.classList.add(game.activePlayer.marker);
+        square.textContent = game.activePlayer.marker;
+        board[index] = game.activePlayer.marker;
+        game.openSquares -= 1;
+      }
+
+      if (game.gameWon === false && game.openSquares > 0) {
+        game.nextPlayer();
+      } else {
+        // game.declareTie();
+      }
     });
     squares.appendChild(square);
   });
@@ -32,5 +41,19 @@ const game = (() => {
   let gameWon = false;
   let openSquares = 9;
 
-  return { activePlayer, gameWon, openSquares };
+  let dialogue = document.querySelector(".dialogue");
+  let player = document.querySelector(".player");
+  player.textContent = "Player 1";
+
+  function nextPlayer() {
+    if (this.activePlayer === playerOne) {
+      this.activePlayer = playerTwo;
+      player.textContent = "Player 2";
+    } else {
+      this.activePlayer = playerOne;
+      player.textContent = "Player 1";
+    }
+  }
+
+  return { activePlayer, gameWon, openSquares, nextPlayer };
 })();
